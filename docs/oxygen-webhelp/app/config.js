4a0703eb178b8b95cdb77c071a586ab18707b31e
kacperbojakowski <kacperbojakowski@gmail.com>
2022-03-26 16:22:01 +0100

    Add minor changes

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220326104613';
+                args = '20220326162104';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
