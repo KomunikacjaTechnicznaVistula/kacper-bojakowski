b58e1708b931f386e55e6a74f6049fcf8a129b8a
kacperbojakowski <kacperbojakowski@gmail.com>
2022-03-26 04:45:51 +0100

    Update PDF href

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220326043010';
+                args = '20220326044517';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
