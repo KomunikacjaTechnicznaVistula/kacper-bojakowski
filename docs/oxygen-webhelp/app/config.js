49cd0cb6f8de4caec544a0af40095b3f5d724b2c
kacperbojakowski <kacperbojakowski@gmail.com>
2022-03-26 10:48:03 +0100

    Add descriptions

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220326045341';
+                args = '20220326104613';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {9:-1,5:-1,2:-1,12:-1,7:-1,4:-1,8:-1,11:-1,3:-1,1:-1,6:-1};
+return {2:-1,5:-1,9:-1,7:-1,12:-1,8:-1,11:-1,4:-1,6:-1,3:-1,1:-1};
 });
