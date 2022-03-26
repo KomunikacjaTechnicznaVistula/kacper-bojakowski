09023bff6fccbee69d7ac728650c82e5385e2268
kacperbojakowski <kacperbojakowski@gmail.com>
2022-03-26 16:40:52 +0100

    Update output

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220326162104';
+                args = '20220326164017';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
