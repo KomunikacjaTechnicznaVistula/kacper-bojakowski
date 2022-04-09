f4669554193da838a2a872b34d2e72833db4365f
kacperbojakowski <kacperbojakowski@gmail.com>
2022-04-09 10:49:20 +0200

    Test

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {2:-1,5:-1,9:-1,7:-1,12:-1,8:-1,11:-1,4:-1,6:-1,3:-1,1:-1};
+return {11:1,7:2,13:2,6:2,5:2,9:2,8:2,3:1,12:2,1:-1,4:2,10:1,2:-1};
 });
